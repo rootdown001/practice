95c20ea7e67821e0f16555e69cac39682d6ec67b
rootdown001 <lance.a.585@gmail.com>
2019-10-25 18:05:35 -0700

    add methods after inheritance

--- hello.js
+++ hello.js
@@ -1,15 +1,20 @@
-var count = 1;
-console.log('count: ', count);
-function chunkArrayInGroups(arr, size) {
-  console.log('size: ', size);
-  console.log('arr: ', arr);
-  
-  
-  return arr;
-  
-  
-}
+function Animal() { }
+Animal.prototype.eat = function() { console.log("nom nom nom"); };
 
-chunkArrayInGroups(["a", "b", "c", "d"], 2);
+function Dog() { }
+
+// Add your code below this line
+Dog.prototype = Object.create(Animal.prototype);
+Dog.prototype.constructor = Dog;
+Dog.prototype.bark = function () {
+  console.log("Woof!");
+};
 
 
+
+// Add your code above this line
+
+let beagle = new Dog();
+
+beagle.eat(); // Should print "nom nom nom"
+beagle.bark(); // Should print "Woof!"
