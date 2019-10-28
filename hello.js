33a11003db5b181343b3c8ec4243af7352e81a17
rootdown001 <lance.a.585@gmail.com>
2019-10-27 18:43:15 -0700

    hazards of imperitive code

--- hello.js
+++ hello.js
@@ -1,40 +1,43 @@
-/**
- * A long process to prepare green tea.
- * @return {string} A cup of green tea.
- **/
-const prepareGreenTea = () => 'greenTea';
-
-/**
- * A long process to prepare black tea.
- * @return {string} A cup of black tea.
- **/
-const prepareBlackTea = () => 'blackTea';
-
-/**
- * Get given number of cups of tea.
- * @param {function():string} prepareTea The type of tea preparing function.
- * @param {number} numOfCups Number of required cups of tea.
- * @return {Array<string>} Given amount of tea cups.
- **/
-const getTea = (prepareTea, numOfCups) => {
-  const teaCups = [];
-
-  for(let cups = 1; cups <= numOfCups; cups += 1) {
-    const teaCup = prepareTea();
-    teaCups.push(teaCup);
-  }
-
-  return teaCups;
+// tabs is an array of titles of each site open within the window
+var Window = function(tabs) {
+  this.tabs = tabs; // we keep a record of the array inside the object
 };
 
-// Add your code below this line
+// When you join two windows into one window
+Window.prototype.join = function (otherWindow) {
+  this.tabs = this.tabs.concat(otherWindow.tabs);
+  return this;
+};
 
-const tea4GreenTeamFCC = getTea(prepareGreenTea, 27);
-const tea4BlackTeamFCC = getTea(prepareBlackTea, 13);
+// When you open a new tab at the end
+Window.prototype.tabOpen = function (tab) {
+  this.tabs.push('new tab'); // let's open a new tab for now
+  return this;
+};
 
-// Add your code above this line
+// When you close a tab
+Window.prototype.tabClose = function (index) {
 
-console.log(
-  tea4GreenTeamFCC,
-  tea4BlackTeamFCC
-);
+  // Only change code below this line
+
+  var tabsBeforeIndex = this.tabs.slice(0, index); // get the tabs before the tab
+  var tabsAfterIndex = this.tabs.slice(index + 1); // get the tabs after the tab
+
+  this.tabs = tabsBeforeIndex.concat(tabsAfterIndex); // join them together
+
+  // Only change code above this line
+
+  return this;
+ };
+
+// Let's create three browser windows
+var workWindow = new Window(['GMail', 'Inbox', 'Work mail', 'Docs', 'freeCodeCamp']); // Your mailbox, drive, and other work sites
+var socialWindow = new Window(['FB', 'Gitter', 'Reddit', 'Twitter', 'Medium']); // Social sites
+var videoWindow = new Window(['Netflix', 'YouTube', 'Vimeo', 'Vine']); //  Entertainment sites
+
+// Now perform the tab opening, closing, and other operations
+var finalTabs = socialWindow
+                    .tabOpen() // Open a new tab for cat memes
+                    .join(videoWindow.tabClose(2)) // Close third tab in video window, and join
+                    .join(workWindow.tabClose(1).tabOpen());
+console.log(finalTabs.tabs);
